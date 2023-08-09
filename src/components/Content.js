01ab73b6e94b999d85e28183d2fbcd038259cacc
Silvia-Suarez <silviznatalia@gmail.com>
2023-08-09 12:53:59 -0500

    contentfull integration

--- src/components/Content.js
+++ src/components/Content.js
@@ -1,10 +1,21 @@
-import React from 'react';
-import Promotions from './Promotions';
-import FlightSearch from './FlightSearch';
-import FlightCatalog from './FlightCatalog';
-import Navigation from './Navigation';
-
+import React, { useEffect, useState } from "react";
+import Promotions from "./Promotions";
+import FlightSearch from "./FlightSearch";
+import FlightCatalog from "./FlightCatalog";
+import Navigation from "./Navigation";
+import client from "../contentful";
 function Content() {
+  const [vuelos, setVuelos] = useState([]);
+  useEffect(() => {
+    async function getVuelos() {
+      await client.getEntries({ content_type: "vuelos" }).then((response) => {
+        setVuelos(response.items);
+        console.log(response.items);
+      });
+    }
+    getVuelos();
+  }, []);
+
   return (
     <>
       <Navigation />
---
+++ src/contentful.js
@@ -0,0 +1,8 @@
+import { createClient } from "contentful";
+
+const client = createClient({
+  space: "3eoft87ntj5c",
+  accessToken: "2vTBcIqZeSKpsZOB1fVEtKqMAjVNSOFvAIHTv5aS79o",
+});
+
+export default client;
