326160e8e844bce70a115fbd87f59d2751c23161
K-Viera <kevin.viera.989@gmail.com>
2023-08-12 20:21:04 -0500

    remove old login

--- src/components/Login.js
+++ src/components/Login.js
@@ -19,14 +19,6 @@ function Login() {
   }
   return (
     <div className="login-container">
-      <h2>Iniciar Sesión</h2>
-      <form className='login-form'>
-        <label>Correo Electrónico</label>
-        <input type="email" />
-        <label>Contraseña</label>
-        <input type="password" />
-        <button className="login-button">Iniciar Sesión</button>
-      </form>
       <button onClick={()=> Auth.federatedSignIn()}>normal</button>
       <button onClick={checkUser}>Check User</button>
       <button onClick={signOut}>Sign Out</button>
